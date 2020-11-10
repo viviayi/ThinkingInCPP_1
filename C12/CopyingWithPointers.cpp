9a6fb0e77b9689ce639074295ea96695c88dfef6
Vivia <741173912@qq.com>
2020-11-10 17:45:04 +0800

    1110

---
+++ C12/CopyingWithPointers.cpp
@@ -0,0 +1,8 @@
+// Solving the pointer aliasing problem by
+// duplication what is pointed to during
+// assignment and copy-construction.
+#include "../require.h"
+#include <string>
+#include <iostream>
+using namespace std;
+
---
+++ C12/Integer.cpp
@@ -0,0 +1,141 @@
+// Implementation of overloaded oprators
+#include "Integer.h"
+#include "../require.h"
+
+const Integer operator+(const Integer& left, const Integer& right){
+    return Integer(left.i + right.i);
+}
+
+const Integer operator-(const Integer& left, const Integer& right){
+    return Integer(left.i - right.i);
+}
+
+const Integer operator*(const Integer& left, const Integer& right){
+    return Integer(left.i * right.i);
+}
+
+const Integer operator/(const Integer& left, const Integer& right){
+    require(right.i != 0, "divide by zero");
+    return Integer(left.i / right.i);
+}
+
+const Integer operator%(const Integer& left, const Integer& right){
+    require(right.i != 0, "modulo by zero");
+    return Integer(left.i % right.i);
+}
+
+const Integer operator^(const Integer& left, const Integer& right){
+    return Integer(left.i ^ right.i);
+}
+
+const Integer operator&(const Integer& left, const Integer& right){
+    return Integer(left.i & right.i);
+}
+
+const Integer operator|(const Integer& left, const Integer& right){
+    return Integer(left.i | right.i);
+}
+
+const Integer operator<<(const Integer& left, const Integer& right){
+    return Integer(left.i << right.i);
+}
+
+const Integer operator>>(const Integer& left, const Integer& right){
+    return Integer(left.i >> right.i);
+}
+
+// Assignments modify & return l value:
+Integer& operator+=(Integer& left, const Integer& right){
+    if(&left == &right) {/* self-assignment */}
+    left.i += right.i;
+    return left;
+}
+
+Integer& operator-=(Integer& left, const Integer& right){
+    if(&left == &right) {/* self-assignment */}
+    left.i -= right.i;
+    return left;
+}
+
+Integer& operator*=(Integer& left, const Integer& right){
+    if(&left == &right) {/* self-assignment */}
+    left.i *= right.i;
+    return left;
+}
+
+Integer& operator/=(Integer& left, const Integer& right){
+    require(right.i != 0, "divide by zero");
+    if(&left == &right) {/* self-assignment */}
+    left.i /= right.i;
+    return left;
+}
+
+Integer& operator%=(Integer& left, const Integer& right){
+    require(right.i != 0, "divide by zero");
+    if(&left == &right) {/* self-assignment */}
+    left.i %= right.i;
+    return left;
+}
+
+Integer& operator^=(Integer& left, const Integer& right){
+    if(&left == &right) {/* self-assignment */}
+    left.i ^= right.i;
+    return left;
+}
+
+Integer& operator&=(Integer& left, const Integer& right){
+    if(&left == &right) {/* self-assignment */}
+    left.i &= right.i;
+    return left;
+}
+
+Integer& operator|=(Integer& left, const Integer& right){
+    if(&left == &right) {/* self-assignment */}
+    left.i |= right.i;
+    return left;
+}
+
+Integer& operator>>=(Integer& left, const Integer& right){
+    if(&left == &right) {/* self-assignment */}
+    left.i >>= right.i;
+    return left;
+}
+
+Integer& operator<<=(Integer& left, const Integer& right){
+    if(&left == &right) {/* self-assignment */}
+    left.i <<= right.i;
+    return left;
+}
+
+// Conditional operators return ture/false:
+int operator==(const Integer& left, const Integer& right){
+    return left.i == right.i;
+}
+
+int operator!=(const Integer& left, const Integer& right){
+    return left.i != right.i;
+}
+
+int operator<(const Integer& left, const Integer& right){
+    return left.i < right.i;
+}
+
+int operator>(const Integer& left, const Integer& right){
+    return left.i > right.i;
+}
+
+int operator<=(const Integer& left, const Integer& right){
+    return left.i < right.i;
+}
+
+int operator>=(const Integer& left, const Integer& right){
+    return left.i > right.i;
+}
+
+int operator&&(const Integer& left, const Integer& right){
+    return left.i && right.i;
+}
+
+int operator||(const Integer& left, const Integer& right){
+    return left.i || right.i;
+} ///:~
