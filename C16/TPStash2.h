c24415bbd552401a676f5f59f70d5816ec49b006
Vivia <741173912@qq.com>
2021-02-02 20:07:06 +0800

    0202

--- C16/TPStash2.h
+++ C16/TPStash2.h
@@ -61,8 +61,85 @@ public:
             index -= amout;
             return *this;
         }
-        // Create a new iterator
+        // Create a new iterator that's moved forward
+        iterator operator+(int amount) const{
+            iterator ret(*this);
+            ret += amount; // op+= does bounds check
+            return ret;
+        }
+        T* current() const{
+            return ps.storage[index];
+        }
+        T* operator*() const{
+            return current;
+        }
+        T* operator->() const{
+            require(ps.storage[index] != 0, "PStash::iterator::operator->returns 0");
+            return current();
+        }
+        // Remove the current elemnet:
+        T* remove(){
+            return ps.remove(index);
+        }
+        // Comparison tests for end:
+        bool operator==(const iterator& rv) const{
+            return index == rv.index;
+        }
+        bool operator!=(const iterator& rv) const{
+            return index != rv.index;
+        }
     };
+    iterator begin() {
+        return iterator(*this);
+    }
+    iterator end(){
+        return iterator(*this, true);
+    }
 };
 
+// Destruction of contained objects:
+template<class T, int incr>
+PStash<T, incr>::~PStash(){
+    for(int i = 0; i < next; i++){
+        delete storage[i]; // Null pointers OK
+        storage[i] = 0; // Just to be safe
+    }
+    delete []storage;
+}
+
+template<class T, int incr>
+int PStash<T, incr>::add(T* element){
+    if(next >= quantity)
+        inflate();
+    storage[next++] = element;
+    return(next - 1); // Index number
+}
+
+template<class T, int incr> inline
+T* PStash<T, incr>::operator[](int index) const{
+    require(index >= 0, "PStash::oprator[] index negative");
+    if(index >= next)
+        return 0; // To indicate the end
+    require(storage[index] != 0, "PStash::operator[] returned null pointer");
+    return storage[index];
+}
+
+template<class T, int incr>
+T* PStash<T, incr>::remove(int index){
+    // operator[] performs validity checks:
+    T* v = operator[](index);
+    // "Remove" the pointer:
+    storage[index] = 0;
+    return v;
+}
+
+template<class T, int incr>
+void PStash<T, incr>::inflate(int increase){
+    const int tsz = sizeof(T*);
+    T** st = new T*[quantity + increase];
+    memset(st, 0, (quantity + increase) * tsz);
+    memcpy(st, storage, quantity * tsz);
+    quantity += increase; // Old storage
+    storage = st; // Point to new memory
+}
 #endif // TPSTASH2_H ///:~
